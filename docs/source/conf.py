10a159ec1084edbbee576666762e164a38b47ed2
Willeke A'Campo (linux vdi) <willeke.acampo@nina.no>
2023-11-29 17:06:58 +0100

    debug: build config  point to docs/.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,11 +102,11 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # github pages html theme
-html_theme = "alabaster"
-html_static_path = ["_static"]
+# html_theme = "alabaster"
+# html_static_path = ["_static"]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
